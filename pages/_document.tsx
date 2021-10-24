38491cffe2332801a161bc8ed506b111ffe016ad
Ray Alez <raymestalez@gmail.com>
2021-10-24 16:19:42 +0100

    Clicky analytics.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,14 +29,9 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
           {/* Clicky Analytics */}
-          <a title="Real Time Web Analytics" href="http://clicky.com/101338515">
-            <img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" />
-          </a>
           <script async src="//static.getclicky.com/101338515.js"></script>
           <noscript>
-            <p>
-              <img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101338515ns.gif" />
-            </p>
+            <img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101338515ns.gif" />
           </noscript>
         </body>
       </Html>
