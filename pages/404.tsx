e3ff0d612df909807e88a5da88f0e943e4a31fe9
Ray Alez <raymestalez@gmail.com>
2021-11-05 14:25:42 +0000

    Error pages.

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import Layout from "components/Layout/Layout"
+
+export default function Error() {
+  return (
+    <Layout>
+      <div className="error-page">
+      <b>404 - Page not Found, sorry!</b>
+      <p>You can see all of our posts <a href="/">here</a>.</p>
+      </div>
+    </Layout>
+  )
+}
---
+++ pages/500.tsx
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import Layout from "components/Layout/Layout"
+
+export default function Error() {
+  return (
+    <Layout>
+      <div className="error-page">
+      <b>Server error.</b>
+        <p>Sorry, that's not supposed to happen!</p>
+        <p>If you see this page - please send an email to <b>lumenwrites@gmail.com</b> and let me know.</p>
+        <p>In the email, please mention what you were trying to do when this happened, <br/> that will really help me to find and fix the issue!</p>
+      </div>
+    </Layout>
+  )
+}
